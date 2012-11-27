39620f8d866276e697ec42af1804ac47c76dd2fa
Vincent Paca <vincent.paca@gmail.com>
2012-11-27 14:26:49 +0800

    Add route for assignemtns

--- app/routes/main.rb
+++ app/routes/main.rb
@@ -2,4 +2,9 @@ class Application < Sinatra::Application
   get '/?' do
     haml :index
   end
+
+  get '/assignments/:assignment' do
+    post = File.read("#{settings.public_folder}/posts/assignments/#{params[:assignment]}.md")
+    haml :assignment, :locals => { :post => post }
+  end
 end
